da48e50abfb06934036f83ddd342032133942fe9
thanhdungfb <thanhdungfb@gmail.com>
2018-05-06 01:43:34 +0700

    update the event

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,38 @@ import './App.css';
 import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('this.state:', this.state);
+    }
+
     render() {
         return (
             <div className="App">
 
                 <div className="App-title">Music Master</div>
-
                 <FormGroup>
                     <InputGroup>
-                        <FormControl 
-                            type="text" 
+                        <FormControl
+                            type="text"
                             placeholder="Search for an Artist"
-                        />
-                        <InputGroup.Addon>
+                            query={this.state.query}
+                            onChange={(event) => {
+                            this.setState({query: event.target.value});
+                        }}
+                            onKeyPress={(event) => {
+                            if(event.key === 'Enter'){
+                                this.search();
+                            }
+                        }}/>
+                        <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
 
